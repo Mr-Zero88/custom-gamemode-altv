26ed3c298aeb0c40ba4717c841df08f1348092fb
Mats Huesmann <huesmann.mats@gmail.com>
2025-01-29 22:58:57 +0100

    Change event listener from onServer to onRpc for character selection

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,6 +1,6 @@
 import * as alt from 'alt-client';
 
-alt.onServer('view:characterSelection:show', () => {
+alt.onRpc('view:characterSelection:show', () => {
     console.log('Showing character selection.');
     let view = new alt.WebView('http://resource/client/views/CharacterSelection/index.html');
     view.focus();
