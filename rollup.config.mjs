4e0743e6f17f3be167f5de68fd1f63a3af6f8b29
Mats Huesmann <huesmann.mats@gmail.com>
2025-01-29 22:44:10 +0100

    build client

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,20 +1,25 @@
-// import typescript from "rollup-plugin-typescript2";
-// import { terser } from "rollup-plugin-terser";
-
 import typescript from 'rollup-plugin-typescript2';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 
-const input = "src/server/index.ts";
-const output = "dist/server/index.js";
-
-export default {
-    input: [input],
-    output: {
-        file: output,
-        format: 'esm',
+export default [
+    {
+        input: "src/client/index.ts",
+        output: {
+            file: "dist/client/index.js",
+            format: 'esm',
+        },
+        external: ['alt-server'],
+        plugins: [typescript(), nodeResolve(), commonjs(), terser()],
     },
-    external: ['alt-server', 'fs', 'path'],
-    plugins: [typescript(), nodeResolve(), commonjs(), terser()],
-};
+    {
+        input: "src/server/index.ts",
+        output: {
+            file: "dist/server/index.js",
+            format: 'esm',
+        },
+        external: ['alt-server', 'fs', 'path'],
+        plugins: [typescript(), nodeResolve(), commonjs(), terser()],
+    }
+]
