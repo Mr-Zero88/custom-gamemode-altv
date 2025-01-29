52615268ef82f3a28ada71b246da1efb98af2eb4
Patrick <blue-ice80@o2online.de>
2025-01-30 00:50:12 +0100

    Add 'getPos' command to retrieve and display player position and rotation

---
+++ src/server/commands/getPos.ts
@@ -0,0 +1,10 @@
+import * as alt from 'alt-server';
+import * as chat from 'vchat';
+
+export default {
+    name: 'getPos',
+    description: 'Get the current position of the player.',
+    execute: async (player, args) => {
+        chat.send(player, `Position: ${player.pos.x}, ${player.pos.y}, ${player.pos.z}, Rotation: ${player.rot.x}, ${player.rot.y}, ${player.rot.z}`);
+    },
+}
