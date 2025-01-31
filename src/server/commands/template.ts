4d984a16d9d8a8f1eb81cf7a5f45f009d1af0c16
Mats Huesmann <huesmann.mats@gmail.com>
2025-01-31 14:45:39 +0100

    Add template command structure for future development

---
+++ src/server/commands/template.ts
@@ -0,0 +1,12 @@
+import * as alt from 'alt-server';
+import * as chat from 'alt:vchat';
+import { Command } from '..';
+
+export default {
+    name: 'template',
+    description: 'Template command.',
+    parameters: [],
+    execute: async (player, args) => {
+
+    },
+} as Command;
